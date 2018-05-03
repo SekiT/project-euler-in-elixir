85b2891e8fc162d0b099297169c0bcfe988cc941
Takaaki Seki <takaaki.seki@access-company.com>
2018-05-03 20:31:30 +0900

    Integer.gcd/2 works great

--- problem5.ex
+++ problem5.ex
@@ -1,11 +1,3 @@
-Enum.reduce(1..20, {[], 1}, fn(n, {factors, product}) ->
-  new_factor =
-    Enum.reduce(factors, n, fn
-      (factor, divided_n) when rem(divided_n, factor) == 0 ->
-        div(divided_n, factor)
-      (_, divided_n) ->
-        divided_n
-    end)
-  {[new_factor | factors], product * new_factor}
-end)
-|> case do {_, product} -> IO.puts(product) end
+1..20
+|> Enum.reduce(fn(a, b) -> div(a * b, Integer.gcd(a, b)) end)
+|> IO.puts()
