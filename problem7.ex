d3b41f216f1bc865aa251c8970d4a2adb5307a2e
Takaaki Seki <takaaki.seki@access-company.com>
2018-05-04 23:40:40 +0900

    Use ++ to append an element to a list

--- problem7.ex
+++ problem7.ex
@@ -2,7 +2,7 @@ Stream.iterate({[2, 3], 5, 2}, fn {primes, n, adder} ->
   if Enum.any?(primes, &(rem(n, &1) == 0)) do
     {primes, n + adder, 6 - adder}
   else
-    {List.insert_at(primes, -1, n), n + adder, 6 - adder}
+    {primes ++ [n], n + adder, 6 - adder}
   end
 end)
 |> Enum.find(fn {primes, _, _} -> length(primes) == 10_001 end)
