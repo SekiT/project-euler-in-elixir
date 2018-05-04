7bb4f27ba20e4f7f53b72b52b4e0b3372feca697
Takaaki Seki <takaaki.seki@access-company.com>
2018-05-04 23:43:07 +0900

    Solve problem7 a bit faster

--- problem7.ex
+++ problem7.ex
@@ -1,8 +1,17 @@
 Stream.iterate({[2, 3], 5, 2}, fn {primes, n, adder} ->
-  if Enum.any?(primes, &(rem(n, &1) == 0)) do
-    {primes, n + adder, 6 - adder}
-  else
+  sqrt = :math.sqrt(n)
+  prime? = Enum.reduce_while(primes, true, fn
+    (prime, _) when rem(n, prime) == 0 ->
+      {:halt, false}
+    (prime, _) when prime >= sqrt ->
+      {:halt, true}
+    (_, _) ->
+      {:cont, true}
+  end)
+  if prime? do
     {primes ++ [n], n + adder, 6 - adder}
+  else
+    {primes, n + adder, 6 - adder}
   end
 end)
 |> Enum.find(fn {primes, _, _} -> length(primes) == 10_001 end)
