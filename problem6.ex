397c954f4e549fda3b04cba748c2f368d6c340b3
Takaaki Seki <takaaki.seki@access-company.com>
2018-05-10 22:16:31 +0900

    Make problem6.ex more readable and simple

--- problem6.ex
+++ problem6.ex
@@ -1,9 +1,4 @@
-sum_of_squares = fn n ->
-  div(n * (n + 1) * (2 * n + 1), 6)
-end
-sum = fn n ->
-  div(n * (n + 1), 2)
-end
 n = 100
-s = sum.(n)
-IO.puts(s * s - sum_of_squares.(n))
+sum = div(n * (n + 1), 2)
+sum_of_squares = div(n * (n + 1) * (2 * n + 1), 6)
+IO.puts(sum * sum - sum_of_squares)
